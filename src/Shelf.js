0468a5cdd5c216825aa511802454d9b8e2518406
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-08-09 00:34:36 +0200

    feat: Add Shelf

---
+++ src/Shelf.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import propTypes from 'prop-types'
+import Book from './Book'
+
+class Shelf extends Component {
+  render() {
+    const {shelfName, books, moveBook, removeBook}= this.props;
+
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{shelfName}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {books.map((book) => (
+              <li key={book.id}><Book book={book} moveBook={moveBook} removeBook={removeBook}/></li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+Shelf.propTypes = {
+  shelfName: propTypes.string.isRequired,
+  books: propTypes.array.isRequired,
+  moveBook: propTypes.func.isRequired,
+  removeBook: propTypes.func.isRequired
+};
+
+export default Shelf
