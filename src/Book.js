6e9dd8fedff9c377b70c7aa85917a92a3b76eb49
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-08-09 11:58:02 +0200

    Make a book's shelf defaultly selected in book's dropdown list

--- src/Book.js
+++ src/Book.js
@@ -19,7 +19,7 @@ class Book extends Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 188, backgroundImage:`url("${book.imageLinks.thumbnail}")` }}></div>
             <div className="book-shelf-changer">
-              <select onChange={this.handleSelection}>
+              <select defaultValue={book.shelf} onChange={this.handleSelection}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
