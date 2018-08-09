fcb9a0ad3cf57e4371fd428defecec3969f2e0f2
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-08-09 13:05:34 +0200

    feat: Add search input field

---
+++ src/SearchInput.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import propTypes from 'prop-types'
+
+class SearchInput extends Component {
+  render() {
+    const {searchTerm, changeSearchTerm} = this.props;
+
+    return (
+      <div className="search-books-bar">
+        <Link to="/" className="close-search">Close</Link>
+        
+        <div className="search-books-input-wrapper">
+          <input type="text" defaultValue={searchTerm} onChange={changeSearchTerm} placeholder="Search by title or author" />
+        </div>
+      </div>
+    )
+  }
+}
+
+SearchInput.propTypes = {
+  searchTerm: propTypes.string.isRequired,
+  changeSearchTerm: propTypes.func.isRequired
+}
+
+export default SearchInput
