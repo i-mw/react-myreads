afdf0c2dcd49f3387f5b1edf8be4c53803d1be0c
Mustafa Wahba <mustafa.m.wahba@gmail.com>
2018-08-11 21:04:50 +0200

    docs: Comment document NotFound component

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+/**
+ * @constructor
+ * @description NotFound component representing the 404 page
+ */
 function NotFound() {
   return (
     <h1 style={{color: 'red', textAlign: 'center', marginTop: '100px'}}>Not Found 404</h1>
